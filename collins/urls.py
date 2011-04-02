ea29207067342cf34bf0a6df9377b96c4dea3853
Harris Lapiroff <harris@chromamine.com>
2011-04-02 03:10:19 -0400

    Adding some dummy URLs and views. Still nothing doing.

--- collins/urls.py
+++ collins/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('collins.views',
+	url(r'(?<blog_slug>[\w-]*)/?', 'blog'),
+	url(r'(?<blog_slug>[\w-]*)/(?<post_pk>[0-9]*)/[\w-]*/?', 'post'),
 )
--- collins/views.py
+++ collins/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+def blog(request, blog_slug):
+	pass
+	
+def post(request, blog_slug, post_pk):
+	pass
