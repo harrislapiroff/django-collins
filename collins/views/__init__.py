fdd49220d013b8d53b9ac9a39bd416792d4f0093
Harris Lapiroff <harris@chromamine.com>
2011-04-02 21:14:24 -0400

    Stylesheet updates, adding RequestContext to views.

--- collins/views/__init__.py
+++ collins/views/__init__.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def home(request):
 	# TODO: this should be a three-way switch
 	# 1: if a primary blog is set on the site, run the blog view
 	# 2: otherwise, if a user is not logged display a register/sign in page
 	# 3: if the user is logged in and there is no primary blog, redirect to user interface
-	return render_to_response('collins/user/home.html')
+	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
--- collins/views/user.py
+++ collins/views/user.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 def register(request):
 	# TODO: implement
-	return render_to_response('collins/user/home.html')
+	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def dashboard(request):
 	# TODO: implement
-	return render_to_response('collins/user/home.html')
+	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def edit_post(request, post_pk):
 	# TODO: implement
-	return render_to_response('collins/user/home.html')
+	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def edit_blog(request, blog_slug):
 	# TODO: implement
-	return render_to_response('collins/user/home.html')
+	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
