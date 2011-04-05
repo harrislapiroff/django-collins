5715042c72b059e3a7b51af3658c5c15ecfdcdbc
Harris Lapiroff <harris@chromamine.com>
2011-04-05 02:17:37 -0400

    Minor style tweaks. Added user registration.

--- collins/views/user.py
+++ collins/views/user.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from collins.models import Blog, User
 
 def register(request):
-	# TODO: implement
-	return render_to_response('collins/user/home.html', {}, context_instance=RequestContext(request))
+	if request.user.is_authenticated(): # if the user is already logged in
+		return HttpResponseRedirect(reverse('dashboard'))
+	elif not request.method == 'POST': # if the form has not been submitted yet
+		return render_to_response('collins/user/register.html', {'form': UserCreationForm()}, context_instance=RequestContext(request))
+	else:
+		form = UserCreationForm(request.POST)
+		if not form.is_valid():
+			return render_to_response('collins/user/register.html', {'form': form}, context_instance=RequestContext(request))
+		else:
+			user = form.save()
+			user = authenticate(username = user.username, password = form.data['password1'])
+			login(request, user)
+			return HttpResponseRedirect(reverse('create_blog'))
 
 @login_required
 def dashboard(request):
